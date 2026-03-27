2b4cbaf10daf7f221ff5bb94ad60b1eb3f2d00d1
AndreGomesN <gomes10andre@gmail.com>
2026-03-26 22:55:04 -0300

    style: fix lint issues (W291, E122)

--- tests/unit_tests/tests_ingestion_cardiovascular.py
+++ tests/unit_tests/tests_ingestion_cardiovascular.py
@@ -77,7 +77,7 @@ class PySparkTest(unittest.TestCase):
     def test_rename_columns(self):
         data = PySparkTest.dataframe_mock(self.spark)
         standardize = rename_columns(data)
-        self.assertListEqual(standardize.columns, ["General_Health", "Checkup", "Exercise", "Heart_Disease", "Skin_Cancer", "Other_Cancer", "Depression", "Diabetes", "Arthritis", "Sex","Age_Category", "Height_cm", "Weight_kg", "BMI", "Smoking_History", "Alcohol_Consumption", "Fruit_Consumption", "Green_Vegetables_Consumption", "FriedPotato_Consumption"])
+        self.assertListEqual(standardize.columns, ["General_Health", "Checkup", "Exercise", "Heart_Disease", "Skin_Cancer", "Other_Cancer", "Depression", "Diabetes", "Arthritis", "Sex", "Age_Category", "Height_cm", "Weight_kg", "BMI", "Smoking_History", "Alcohol_Consumption", "Fruit_Consumption", "Green_Vegetables_Consumption", "FriedPotato_Consumption"])
 
     def test_save_delta(self):
         data = PySparkTest.dataframe_mock(self.spark)
@@ -112,4 +112,3 @@ class PySparkTest(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-    
